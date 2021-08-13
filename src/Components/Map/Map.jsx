39173c2e96aee6ace27ab923e84b74ddf5930838
Meepo <Muniss138@gmail.com>
2021-08-13 23:46:59 +0500

    first commit

---
+++ src/Components/Map/Map.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Map = () => {
+  return <h1>I'm Map</h1>;
+};
+
+export default Map;
---
+++ src/Components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlaceDetails = () => {
+  return <h1>I'm PlaceDetails</h1>;
+};
+
+export default PlaceDetails;
